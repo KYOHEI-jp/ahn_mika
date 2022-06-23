562b40d9405f59f3763551aa24f3ff8b3b5045ac
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-06-23 20:59:59 +0900

    色追加

---
+++ lib/constants.dart
@@ -0,0 +1,24 @@
+/*
+ * Created by KYOHEI WATANABE on 2022/06/23 20:46
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 2022/06/21 20:07
+ */
+
+import 'package:flutter/material.dart';
+
+const dark = Color(0xFF1D1E33);
+const white = Color(0xFFFFFFFF);
+const antiqueWhite = Color(0xFFFAEBD7);
+const wheat = Color(0xFFF5DEB3);
+const beige = Color(0xFFF5F5DC);
+const linen = Color(0xFFFAF0E6);
+const oldLace = Color(0xFFFDF5E6);
+const lightGoldenRodYellow = Color(0xFFFAFAD2);
+const cornSilk = Color(0xFFFFF8DC);
+const ivory = Color(0xFFFFFFF0);
+
+
+const smallTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0XFF8D8E98),
+);
