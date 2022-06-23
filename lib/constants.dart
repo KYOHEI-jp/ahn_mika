415d7ab46b4f4889be1c6092e0daf579f1374d7f
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-06-23 21:13:19 +0900

    色追加

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,16 +6,30 @@
 
 import 'package:flutter/material.dart';
 
+
+// Colorシリーズ
 const dark = Color(0xFF1D1E33);
 const white = Color(0xFFFFFFFF);
 const antiqueWhite = Color(0xFFFAEBD7);
 const wheat = Color(0xFFF5DEB3);
 const beige = Color(0xFFF5F5DC);
 const linen = Color(0xFFFAF0E6);
+const moccasin = Color(0xFFFFE4B5);
+const blancheDalmond = Color(0xFFFFEBCD);
+const navajoWhite = Color(0xFFFFDEAD);
 const oldLace = Color(0xFFFDF5E6);
 const lightGoldenRodYellow = Color(0xFFFAFAD2);
 const cornSilk = Color(0xFFFFF8DC);
 const ivory = Color(0xFFFFFFF0);
+const whiteSmoke = Color(0xFFF5F5F5);
+const snow = Color(0xFFFAFAFA);
+const ghostWhite = Color(0xFFF8F8FF);
+const floralWhite = Color(0xFFFFFAF0);
+const papayaWhip = Color(0xFFFFFFD5);
+const mistyRose= Color(0xFFFFE4E1);
+const bisque = Color(0xFFFFE4C4);
+const lemonChiffon = Color(0xFFFFFACD);
+const lightYellow = Color(0xFFFFFFE0);
 
 
 const smallTextStyle = TextStyle(
